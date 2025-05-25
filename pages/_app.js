6c9d313d0f9964f24a6e3035bb8a410d7435762c
Diabolichead3094 <madhuluck17@gmail.com>
2025-05-25 23:58:43 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
+javascript
 import '../styles/globals.css'
+import { useState, useEffect } from 'react'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    // Check if user is logged in
+    const token = localStorage.getItem('token')
+    if (token) {
+      setUser({ token })
+    }
+  }, [])
+
+  return <Component {...pageProps} user={user} setUser={setUser} />
 }
